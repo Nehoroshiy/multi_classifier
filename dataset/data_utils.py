db4f548c9f8d319541ceb056ebda7157c3f8dde8
Constantine <nehoroshiy.ya@ya.ru>
2015-12-11 18:24:13 +0300

    add data utils file

--- dataset/data_utils.py
+++ dataset/data_utils.py
@@ -4,6 +4,7 @@ import sys
 import os
 import gc
 
+
 def load_CIFAR_batch(filename):
     """ load single batch of cifar """
     with open(filename, 'rb') as f:
@@ -53,3 +54,43 @@ def load_CIFAR10(ROOT):
         for arr, fname in zip([x_all, y_all, Xte, Yte], np_names):
             arr.astype(np.uint8).tofile(os.path.join(ROOT, fname))
         return x_all, y_all, Xte, Yte
+
+
+def get_cifar10_data(num_training=49000, num_validation=1000, num_test=1000):
+    """
+    Load the CIFAR-10 dataset from disk and perform preprocessing to prepare
+    it for the linear classifier. These are the same steps as we used for the
+    SVM, but condensed to a single function.
+    """
+    # Load the raw CIFAR-10 data
+    cifar10_dir = 'dataset/datasets/cifar-10-batches-py'
+    X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+
+    # subsample the data
+    mask = np.arange(num_training, num_training + num_validation)
+    X_val = X_train[mask]
+    y_val = y_train[mask]
+    mask = np.arange(num_training)
+    X_train = X_train[mask]
+    y_train = y_train[mask]
+    mask = np.arange(num_test)
+    X_test = X_test[mask]
+    y_test = y_test[mask]
+
+    # Preprocessing: reshape the image data into rows
+    X_train = np.reshape(X_train, (X_train.shape[0], -1))
+    X_val = np.reshape(X_val, (X_val.shape[0], -1))
+    X_test = np.reshape(X_test, (X_test.shape[0], -1))
+
+    # Normalize the data: subtract the mean image
+    mean_image = np.mean(X_train, axis = 0)
+    X_train -= mean_image
+    X_val -= mean_image
+    X_test -= mean_image
+
+    # add bias dimension and transform into columns
+    X_train = np.hstack([X_train, np.ones((X_train.shape[0], 1))]).T
+    X_val = np.hstack([X_val, np.ones((X_val.shape[0], 1))]).T
+    X_test = np.hstack([X_test, np.ones((X_test.shape[0], 1))]).T
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
