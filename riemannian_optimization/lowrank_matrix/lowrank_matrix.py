35ca32da14092d09bf1e4615a9d2dec393a2fdfb
Constantine <nehoroshiy.ya@ya.ru>
2015-12-17 20:11:52 +0300

    reformat file according to the pep styleguide. Add trace and scalar_product methods to ManifoldElement

--- riemannian_optimization/lowrank_matrix/lowrank_matrix.py
+++ riemannian_optimization/lowrank_matrix/lowrank_matrix.py
@@ -39,8 +39,6 @@ from riemannian_optimization.utils.approx_utils import csvd
 def orth(a, eps=1e-12):
     return np.linalg.norm(a.T.dot(a) - np.eye(a.shape[1])) < eps * np.sqrt(a.shape[1])
 
-#return np.allclose(a.T.dot(a), np.eye(a.shape[1]))
-
 
 def torth(a):
     return np.allclose(a.dot(a.T), np.eye(a.shape[0]))
@@ -88,17 +86,15 @@ class ManifoldElement(object):
 
     """
     def __init__(self, data, r=None):
-        if type(data) == ManifoldElement:
+        if isinstance(data, ManifoldElement):
             if r is None:
                 ManifoldElement.__init__(self, data, data.r)
-                return
             elif r <= data.r:
                 self.u = data.u[:, :r].copy()
                 self.s = data.s[:r].copy()
                 self.v = data.v[:r, :].copy()
                 self.shape = data.shape
                 self.r = r
-                return
             else:
                 self.u = np.zeros(data.shape[0], r)
                 self.s = np.zeros(r)
@@ -108,13 +104,11 @@ class ManifoldElement(object):
                 self.v[:data.r, :] = data.v
                 self.shape = data.shape
                 self.r = r
-                return
-        elif type(data) == np.ndarray:
+        elif isinstance(data, np.ndarray):
             self.r = min(data.shape) if r is None else min(min(data.shape), r)
             self.shape = data.shape
             self.u, self.s, self.v = csvd(data, self.r)
-            return
-        elif type(data) == tuple:
+        elif isinstance(data, tuple):
             if len(data) == 2:
                 # we have u, v matrices
                 u, v = data
@@ -125,7 +119,6 @@ class ManifoldElement(object):
                 self.shape = (u.shape[0], v.shape[1])
                 self.u, self.s, self.v = u, np.ones(self.r), v
                 self.balance()
-                return
             elif len(data) == 3:
                 u, s, v = data
                 if len(s.shape) == 2:
@@ -142,7 +135,6 @@ class ManifoldElement(object):
                 if not np.allclose(self.s.argsort(), np.arange(self.s.size)[::-1]):
                     self.rearrange()        # We lost ordering of s while performing addition
                 self.balance()              # We lost orthogonality while performing hadamard
-                return
             else:
                 raise ValueError("Arguments in tuple are not supported")
         else:
@@ -161,7 +153,7 @@ class ManifoldElement(object):
         self.u = self.u[:, permutation]
         self.v = self.v[permutation, :]
         self.s = self.s[permutation]
-        return
+        return None
 
     def _balance_right(self):
         """
@@ -173,7 +165,7 @@ class ManifoldElement(object):
         """
         u, self.s, self.v = csvd(np.diag(self.s).dot(self.v))
         self.u = self.u.dot(u)
-        return
+        return None
 
     def _balance_left(self):
         """
@@ -185,7 +177,7 @@ class ManifoldElement(object):
         """
         self.u, self.s, v = csvd(self.u.dot(np.diag(self.s)))
         self.v = v.dot(self.v)
-        return
+        return None
 
     def _balance(self):
         """
@@ -198,7 +190,7 @@ class ManifoldElement(object):
         self.u = self.u.dot(np.diag(self.s).dot(mid))
         self.u, self.s, v = csvd(self.u)
         self.v = v.dot(self.v)
-        return
+        return None
 
     def balance(self):
         """
@@ -241,10 +233,10 @@ class ManifoldElement(object):
         return left_balanced and right_balanced and sigma_sorted
 
     def __add__(self, other):
-        if type(other) is np.ndarray:
+        if isinstance(other, np.ndarray):
             other = ManifoldElement(other, self.r)
             return self.__add__(other)
-        elif type(other) is ManifoldElement:
+        elif isinstance(other, ManifoldElement):
             u = np.hstack((self.u, other.u))
             s = np.concatenate((self.s, other.s))
             v = np.vstack((self.v, other.v))
@@ -262,23 +254,23 @@ class ManifoldElement(object):
                 "operation is not supported for ManifoldElement and {}".format(type(other)))
 
     def __radd__(self, other):
-        if type(other) not in [np.ndarray, ManifoldElement]:
+        if not isinstance(other, np.ndarray) or not isinstance(other, ManifoldElement):
             raise ValueError(
                 "operation is not supported for ManifoldElement and {}".format(type(other)))
         return self.__add__(other)
 
     def __sub__(self, other):
-        if type(other) is np.ndarray:
+        if isinstance(other, np.ndarray):
             other = ManifoldElement(other, self.r)
             return self.__sub__(other)
-        elif type(other) is ManifoldElement:
+        elif isinstance(other, ManifoldElement):
             return self + (-other)
         else:
             raise ValueError(
                 "operation is not supported for ManifoldElement and {}".format(type(other)))
 
     def __rsub__(self, other):
-        if type(other) not in [np.ndarray, ManifoldElement]:
+        if not isinstance(other, np.ndarray) or not isinstance(other, ManifoldElement):
             raise ValueError(
                 "operation is not supported for ManifoldElement and {}".format(type(other)))
         return self.__sub__(other)
@@ -289,16 +281,19 @@ class ManifoldElement(object):
     def __mul__(self, other):
         if np.isscalar(other):
             return ManifoldElement((self.u, self.s * other, self.v))
-        if self.shape != other.shape:
-            raise ValueError("Shapes {} and {} are not equal.".format(self.shape, other.shape))
-        u = np.zeros((self.shape[0], self.r * other.r))
-        for i in range(self.shape[0]):
-            u[i, :] = np.kron(self.u[i, :], other.u[i, :])
-        s = np.kron(self.s, other.s)
-        v = np.zeros((self.r * other.r, self.shape[1]))
-        for i in range(self.shape[1]):
-            v[:, i] = np.kron(self.v[:, i], other.v[:, i])
-        return ManifoldElement((u, s, v))
+        if isinstance(other, ManifoldElement):
+            if self.shape != other.shape:
+                raise ValueError("Shapes {} and {} are not equal.".format(self.shape, other.shape))
+            u = np.zeros((self.shape[0], self.r * other.r))
+            for i in range(self.shape[0]):
+                u[i, :] = np.kron(self.u[i, :], other.u[i, :])
+            s = np.kron(self.s, other.s)
+            v = np.zeros((self.r * other.r, self.shape[1]))
+            for i in range(self.shape[1]):
+                v[:, i] = np.kron(self.v[:, i], other.v[:, i])
+            return ManifoldElement((u, s, v))
+        else:
+            raise ValueError("mul supports only scalars and ManifoldElement instances")
 
     def __rmul__(self, other):
         # TODO do we need this right-side operator?
@@ -332,13 +327,13 @@ class ManifoldElement(object):
         """
         if self.shape[1] != other.shape[0]:
             raise ValueError("shapes must match!")
-        if type(other) is ManifoldElement:
+        if isinstance(other, ManifoldElement):
             r_mid = np.dot(np.diag(self.s), self.v.dot(other.u)).dot(np.diag(other.s))
             u, s, v = np.linalg.svd(r_mid, full_matrices=False)
             u = self.u.dot(u)
             v = v.dot(other.v)
             return ManifoldElement((u, s, v))
-        elif type(other) is np.ndarray:
+        elif isinstance(other, np.ndarray):
             # TODO there are some ways to optimize (all balance work gone to constructor)
             return ManifoldElement((self.u, self.s, self.v.dot(other)))
         elif sp.sparse.issparse(other):
@@ -363,9 +358,9 @@ class ManifoldElement(object):
         """
         if self.shape[0] != other.shape[1]:
             raise ValueError("shapes must match!")
-        if type(other) is ManifoldElement:
+        if isinstance(other, ManifoldElement):
             return other.dot(self)
-        elif type(other) is np.ndarray:
+        elif isinstance(other, np.ndarray):
             return ManifoldElement((other.dot(self.u), self.s, self.v))
         elif sp.sparse.issparse(other):
             return ManifoldElement((csr_matrix(other).dot(self.u), self.s, self.v))
@@ -382,6 +377,35 @@ class ManifoldElement(object):
             full matrix, given by approximation
         """
         return self.u.dot(np.diag(self.s)).dot(self.v)
+
+    def trace(self):
+        """
+        Returns trace of a matrix
+
+        Returns
+        -------
+        out: float
+            trace of a matrix
+        """
+        return np.einsum('ij, ji ->', self.u * self.s, self.v)
+
+    def scalar_product(self, other):
+        """
+        Returns scalar product of two ManifoldElements
+
+        Parameters
+        ----------
+        other : ManifoldElement
+            element for which scalar product will be computed
+
+        Returns
+        -------
+        out: float
+            scalar product of two ManifoldElements
+        """
+        if self.shape != other.shape:
+            raise ValueError('Shapes mismatch.')
+        return self.dot(other.T).trace()
 
     def frobenius_norm(self):
         """
@@ -418,11 +442,6 @@ class ManifoldElement(object):
                                            self.v[:, sigma_set[1][idx: idx + count]])
         return csr_matrix(coo_matrix((data, tuple(sigma_set)), shape=self.shape))
 
-        #res = lil_matrix(self.shape)
-        #for (i, j) in zip(*sigma_set):
-        #    res[i, j] = np.dot(self.u[i, :] * self.s, self.v[:, j])
-        #return csr_matrix(res)
-
     def isclose(self, other, tol=1e-9):
         """
         Check if element is close to another in frobenius norm.
@@ -439,7 +458,7 @@ class ManifoldElement(object):
         status: bool
             flag indicates that to elements are close or not
         """
-        if type(other) is not ManifoldElement:
+        if isinstance(other, ManifoldElement):
             raise ValueError("we can measure closeness only between ManifoldElements")
         largest_norm = max(self.frobenius_norm(), other.frobenius_norm())
         if largest_norm == 0:
