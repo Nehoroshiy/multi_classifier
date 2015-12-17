4d5be3ebffc2ef4a779b19b3477c9cf5cbf0231f
Constantine <nehoroshiy.ya@ya.ru>
2015-12-17 23:10:39 +0300

    Add first version of conjugated gradients

--- riemannian_optimization/sparse/gd/conjugated_gradients.py
+++ riemannian_optimization/sparse/gd/conjugated_gradients.py
@@ -29,15 +29,14 @@ THE SOFTWARE.
 import numpy as np
 import scipy as sp
 
-from scipy import sparse, linalg, optimize
+from scipy import sparse
 
-from scipy.linalg import rq
-from numpy.linalg import svd, qr, norm
-from scipy.optimize import minimize_scalar
-from scipy.sparse import csr_matrix, csc_matrix, coo_matrix, linalg
-
-from riemannian_optimization.utils.approx_utils import csvd
+from scipy.sparse import linalg, csr_matrix, csc_matrix
+from scipy.optimize import minimize_scalar, line_search
 from riemannian_optimization.lowrank_matrix import ManifoldElement
+from riemannian_optimization.sparse.utils.retractions import svd_retraction
+from riemannian_optimization.sparse.utils.loss_functions import delta_on_sigma_set
+from riemannian_optimization.sparse.utils.projections import TangentVector, riemannian_grad_partial
 
 
 def conjugate_direction(x_prev, grad_prev, dir_prev, x, grad):
@@ -45,5 +44,55 @@ def conjugate_direction(x_prev, grad_prev, dir_prev, x, grad):
     dir_prev_trans = dir_prev.transport(x)
 
     delta = grad - grad_prev_trans
-    beta = max(0, delta.scalar_product(grad) / grad_prev_trans.frobenius_norm**2)
+    beta = max(0, delta.release().scalar_product(grad.release()) / grad_prev_trans.release().frobenius_norm()**2)
     dir_ = -grad + beta * dir_prev_trans
+
+    angle = grad.release().scalar_product(dir_.release()) / np.sqrt(dir_.release().frobenius_norm()**2 * grad.release().frobenius_norm()**2)
+    if angle <= 0.1:
+        dir_ = grad
+    return dir_
+
+
+def closed_form_initial_guess(vec, delta, sigma_set):
+    n_mat = csc_matrix(vec.release().evaluate(sigma_set)).T
+    trace_first = n_mat.multiply(delta.T).sum()
+    trace_second = n_mat.multiply(n_mat.T).sum()
+    return trace_first / trace_second
+
+
+def cg(a, sigma_set, r, maxiter=900, eps=1e-9):
+    def cost_function(param):
+        temp = x + param * conj_dir.release()
+        return 0.5 * sp.sparse.linalg.norm(temp.evaluate(sigma_set) - a) ** 2
+
+    def cost_raw(x_val):
+        return 0.5 * sp.sparse.linalg.norm(x_val.evaluate(sigma_set) - a) ** 2
+
+
+    x = ManifoldElement.rand(a.shape, r)
+    x_prev = x
+    error_history = []
+    conj_dir, conj_dir_prev = TangentVector.zero(x), TangentVector.zero(x)
+    grad = -TangentVector(x, riemannian_grad_partial(x, a, sigma_set, as_manifold_elements=True))
+    for it in range(maxiter):
+        delta = delta_on_sigma_set(x, a, sigma_set)
+        grad_prev, grad = grad, -TangentVector(x, riemannian_grad_partial(x, a, sigma_set, grad=delta, as_manifold_elements=True))
+        error_history.append(grad.release().frobenius_norm())
+        if error_history[-1] < eps:
+            print('Small grad norm {} is reached at iteration {}'.format(error_history[-1], it))
+            return x, it, error_history
+        conj_dir_prev, conj_dir = conj_dir,\
+                                  conjugate_direction(x_prev, grad_prev, conj_dir_prev, x, grad)
+        alpha = minimize_scalar(fun=cost_function, bounds=(0., 10.), method='bounded')['x']
+        #alpha_init = closed_form_initial_guess(conj_dir, delta, sigma_set)
+        #alpha = line_search(cost_function, xk=alpha_init, )
+        m = 0
+        for i in range(20):
+            if cost_raw(x) - cost_raw(svd_retraction(x + 0.5**m * alpha * conj_dir.release(), r)) >= \
+                    -0.0001 * 0.5**m * alpha * grad.release().scalar_product(conj_dir.release()):
+                m = i
+                break
+        print('iter:{}, alpha: {}, m: {} error: {}'.format(it, alpha, m, error_history[-1]))
+        x_prev, x = x, svd_retraction(x + 0.5**m * alpha * conj_dir.release(), r)
+    print('Error {} is reached at iteration {}. Cannot converge'.format(error_history[-1], maxiter))
+    return x, maxiter, error_history
