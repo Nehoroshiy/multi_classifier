58a84daa505c9a08cf9195b5acf16b05472344be
Constantine <nehoroshiy.ya@ya.ru>
2015-12-17 20:13:14 +0300

    minor changes to Momentum GD

--- riemannian_optimization/sparse/gd/gradient.py
+++ riemannian_optimization/sparse/gd/gradient.py
@@ -147,7 +147,7 @@ def old_momentum_approximate(a, sigma_set, r, maxiter=900, mu=0.9, learn_rate=0.
 
 def momentum_approximate(a, sigma_set, r, maxiter=900, mu=0.85, learn_rate=0.15, eps=1e-9):
     """
-    Approximation of sparse matrix a with gradient descend method.
+    Approximation of sparse matrix a with gradient descend method with momentum update.
     Matrix a is known only at sigma_set indices, at other indices it equals zero.
     Function use Riemannian Gradient Descend with line search to find approximation.
 
@@ -194,9 +194,8 @@ def momentum_approximate(a, sigma_set, r, maxiter=900, mu=0.85, learn_rate=0.15,
         v_released = v.release()
         projection_released = projection.release()
         alpha = minimize_scalar(fun=cost_func, bounds=(0., 10.), method='bounded')['x']
-        v = v + alpha * projection
+        v += alpha * projection
         print('iter:{}, alpha: {}, error: {}'.format(it, alpha, err[-1]))
         x = retraction(x + v.release(), r)
-        #x = retraction(x + alpha * projection, r)
     print('Error {} is reached at iteration {}. Cannot converge'.format(err[-1], maxiter))
     return x, maxiter, err
