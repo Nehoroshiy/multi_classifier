08253db37cf5404e78da230325277e9573fc46b9
Constantine <nehoroshiy.ya@ya.ru>
2015-12-21 06:20:50 +0300

    Delete old gd/mgd/cg code

--- manopt/approximator/gd_approximator.py
+++ manopt/approximator/gd_approximator.py
@@ -25,7 +25,3 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
-
-from gradient import gd_approximate, momentum_approximate
-from conjugated_gradients import cg, old_cg
-from gd import gd
