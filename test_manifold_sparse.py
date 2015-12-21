dfdfc764cb3fb9ddbd57e06c9e55c88c9492b748
Constantine <nehoroshiy.ya@ya.ru>
2015-12-21 17:20:12 +0300

    Rewrite test manifold

--- test_manifold_sparse.py
+++ test_manifold_sparse.py
@@ -29,8 +29,9 @@ THE SOFTWARE.
 import numpy as np
 import scipy as sp
 from matplotlib import pyplot as plt
-from scipy.sparse import csr_matrix, lil_matrix
+from scipy.sparse import csr_matrix, lil_matrix, coo_matrix
 
+from manopt import BlackBox
 from manopt.approximator import CGApproximator, GDApproximator, ManifoldElement, MGDApproximator
 
 
@@ -46,72 +47,36 @@ np.set_printoptions(linewidth=450, suppress=True)
 # 4. Try to test on Hilbert matrix
 # 5. Try to make experimenting easier (unified way to make low-rank matrices,
 #       unified way to perform and compare algorithms and they quality)
-if __name__ == "__main__":
-    shape = (100, 100)
-    r = 2
-    opt_nnz = 10. * r * sum(shape)
-    percent = opt_nnz / np.prod(shape)
-    print('percent: {}'.format(percent))
-    sigma_set = generate_sigma_set(shape, percent)
-    sigma_set[0][:] = sigma_set[0][sigma_set[1].argsort()]
-    sigma_set[1][:] = sigma_set[1][sigma_set[1].argsort()]
-    sigma_set[1][:] = sigma_set[1][sigma_set[0].argsort()]
-    sigma_set[0][:] = sigma_set[0][sigma_set[0].argsort()]
-
-    a_full = 1. * (shape[1]*np.arange(shape[0])[:, None] + np.arange(shape[1]))
-    a_sparse = lil_matrix(shape)
-    for (i, j) in zip(*sigma_set):
-        a_sparse[i, j] = a_full[i, j]
-    a_sparse = csr_matrix(a_sparse)
-    #test_batch_size = 20
-    #maxiter = 200
-    #its = np.array([old_cg(a_sparse, sigma_set, r, maxiter=maxiter)[1] for _ in range(test_batch_size)])
-    #print(its)
-    #print('fault rate: {}'.format(np.average(its == maxiter)))
-    #cProfile.run('x, it, err = gd_approximate(a_sparse, sigma_set, r, maxiter=200)')
-    #x, it, err = gd_approximate(a_sparse, sigma_set, r, maxiter=5)
-    #x, it, err = cg(a_sparse, sigma_set, r, maxiter=600)
-    print('a nnz: {}'.format(a_sparse.size))
-    print('real a norm: {}'.format(np.linalg.norm(a_full)))
 
 
-    """
-    x0=None
-    x = x0
-    maxiter_ordinary = 10
-    for rank in range(1, r):
-        current_maxiter = 1
-        x, it, err = cg_fuck(a_sparse, sigma_set, rank, x0=x, maxiter=int(maxiter_ordinary * current_maxiter), eps=1e-9)
-        if it != int(maxiter_ordinary * current_maxiter):
-            r = rank
-            break
-    print('rank is {}'.format(r))
-    print('x sigma:')
-    print(x.s)
-    print('eps of x - a: {}'.format(np.linalg.norm(x.full_matrix() - a_full) / np.linalg.norm(a_full)))
-    r = 2
-    print('real rank is {}'.format(r))
-    x = ManifoldElement(x, r)
-    x, it, err = cg_fuck(a_sparse, sigma_set, r, x0=x, maxiter=100, eps=1e-14)
-    print('rank is {}'.format(r))
-    """
-    approximator = MGDApproximator()
-    x, it, err = approximator.approximate(a=a_sparse, r=2, sigma_set=sigma_set, eps=1e-14)
-    print('eps of x - a: {}'.format(np.linalg.norm(x.full_matrix() - a_full) / np.linalg.norm(a_full)))
+def gen_sum_func(shape):
+    def sum_func(indices):
+        return indices[:, 0] + (1. / shape[1]) * indices[:, 1]
+    return sum_func
 
 
-    print('full matrix x:')
-    print(x.full_matrix())
+def approx_test_ranks(box, ranks=None, maxiter=900):
+    approximator = CGApproximator()
+    ranks = np.arange(1, 4) if ranks is None else np.asarray(ranks, dtype=int)
+    results = []
+    for r in ranks:
+        opt_nnz = 10. * r * sum(shape)
+        percent = opt_nnz / np.prod(shape)
+        print('percent: {}'.format(percent))
+        sigma_set = generate_sigma_set(box.shape, percent)
+        data = box[sigma_set]
+        a_sparse = csr_matrix(coo_matrix((data, sigma_set), shape=shape))
 
-    print('-'*80)
-
-    print('full matrix a: ')
-    print(a_full)
-
-    print('-'*80)
-
-    print('delta x and a')
-    print(x.full_matrix() - a_full)
-
-    plt.plot(np.arange(len(err))[:], err[:])
+        results.append(approximator.approximate(a_sparse, r, sigma_set, maxiter=maxiter, eps=1e-10))
+    for i, (x, it, err) in enumerate(results):
+        print('eps of x - a: {} at r={}'.format(np.linalg.norm(x.full_matrix() - box[:]) / np.linalg.norm(box[:]), i+1))
+        err = np.pad(err, (0, maxiter - len(err)), mode='constant')
+        plt.plot(np.arange(maxiter), err)
     plt.show()
+    return None
+
+if __name__ == "__main__":
+    shapes = [(n, n) for n in 32*np.arange(1, 4)]
+    ranks = np.arange(1, 5)
+    shape = shapes[-1]
+    approx_test_ranks(BlackBox(gen_sum_func(shape), shape))
